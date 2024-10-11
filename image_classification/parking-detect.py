25281e7fb2a2cad1d01bdc351cf3cf29f0b58f2b
Louis Nguyen <ndmn004@gmail.com>
2024-10-11 18:57:08 +1100

    commit

--- image_classification/parking-detect.py
+++ image_classification/parking-detect.py
@@ -1,6 +1,7 @@
 import os
 from skimage.io import imread
 from skimage.transform import resize
+import numpy as np
 
 # prepare data
 input_dir = './parking-dataset/'
@@ -10,13 +11,16 @@ data = []
 labels = []
 IMG_SIZE = 15
 
-for category in categories:
+for idx, category in enumerate(categories):
 	for file in os.listdir(os.path.join(input_dir, category)):
 		img_path = os.path.join(input_dir, category, file)
 		img = imread(img_path)
 		img = resize(img, (IMG_SIZE, IMG_SIZE))
 		data.append(img.flatten())
-		labels.append(category)
+		labels.append(idx)
+
+data = np.array(data)
+labels = np.array(labels)
 
 # train / test split
 
